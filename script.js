0b1a73b4e29b086ddad20d6d308db76f8c7b1471
JohannH <johann.hertz2009@gmail.com>
2025-11-08 13:14:19 -0500

    create basic structure for Etch a sketch
    create a headline, an input, an enter button and a
    div. When inpued in the input and clicked enter, create
    the  input * input grid of squares. When no number is inputed
    or number is to high(>10), Message for error is shown.

---
+++ script.js
@@ -0,0 +1,29 @@
+const sketchPad = document.querySelector("#sketchPad");
+const input = document.querySelector("#input");
+const enter = document.querySelector("#enter");
+
+enter.addEventListener('click', () => {
+
+    if(input.value < 11 && +input.value !== NaN){
+        sketchPad.textContent = '';
+
+        sketchPad.style.width = `${input.value * 64}px`;
+        sketchPad.style.height = `${input.value * 64}px`;
+
+
+        for(let i = 0; i < input.value * input.value; i++){
+            const square = document.createElement("div");
+    
+            square.classList.add("squareStyle");
+            square.addEventListener('mouseover',() => {
+            square.style.backgroundColor = "black";
+            });
+            sketchPad.appendChild(square);
+            }
+
+    }else{
+        sketchPad.textContent = 'That is not a number, or the number is to high';
+    }
+});
+      
+
